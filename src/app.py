b16fcd9fc0eeb049d50f5597b54f43d020e730c8
Alejo Preciado <alejandro.preciado.123@gmail.com>
2025-04-01 14:59:51 -0500

    cambio puerto

--- src/app.py
+++ src/app.py
@@ -13,4 +13,4 @@ app.register_blueprint(payment_routes)
 
 if __name__ == '__main__':
     # Puedes acceder a las variables de entorno con os.getenv
-    app.run(debug=True, host=os.getenv("FLASK_RUN_HOST", "127.0.0.1"), port=int(os.getenv("FLASK_RUN_PORT", 5000)))
+    app.run(debug=True, host=os.getenv("FLASK_RUN_HOST", "127.0.0.1"), port=int(os.getenv("FLASK_RUN_PORT", 5500)))
